f6168a08bf65abf4553f720e69f744b90dac88e2
vagrant <vagrant@homestead>
2017-01-26 02:57:31 +0000

    Subindo o projeto

---
+++ database/migrations/2017_01_26_002924_create_servidors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServidorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('servidors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('servidor',40);
+            $table->string('ip',15);
+            $table->smallInteger('status');
+            $table->integer('id_usuario')->unsigned()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('servidors');
+    }
+}
---
+++ database/seeds/ServidoresTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ServidoresTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+		DB::table('servidors')->insert([
+			[
+            'servidor'=>'STARTER',
+            'ip' => '164.35.10.6',
+            'status' => 0,
+            'id_usuario'=> 1,
+            'created_at'=> date("Y-m-d H:i:s"),
+            
+        	],
+			[
+            'servidor'=>'ROCK',
+            'ip' => '164.35.10.3',
+            'status' => 0,
+            'id_usuario'=> 1,
+            'created_at'=> date("Y-m-d H:i:s"),
+            
+
+        	],
+			[
+            'servidor'=>'WATER',
+            'ip' => '164.35.10.7',
+            'status' => 0,
+            'id_usuario'=> 1,
+            'created_at'=> date("Y-m-d H:i:s"),
+            
+        	]
+
+        ]);
+    }
+}
